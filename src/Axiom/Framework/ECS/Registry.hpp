3069777cf9a7c74447f81c79a9048286823c118e
Sam <michalconstantine@gmail.com>
2022-06-12 12:49:41 +0200

    Added wip framework structure
    Took 40 seconds

---
+++ src/Axiom/Framework/ECS/Registry.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "Axiom/Core/Array.hpp"
+#include "Axiom/Core/robin_hood.h"
+#include "Axiom/Framework/Object.hpp"
+
+namespace Ax
+{
+	typedef uint32_t EntityID;
+
+	struct ESCRegistry
+	{
+
+
+		EntityID NewEntity()
+		{
+			return 0;
+		}
+
+		bool RemoveEntity(EntityID entity)
+		{
+			return false;
+		}
+
+		template<typename T, typename ... Args>
+		T* AddComponent(EntityID entity, Args... args)
+		{
+			return nullptr;
+		}
+
+		template<typename T>
+		T* RemoveComponent()
+		{
+			return nullptr;
+		}
+
+		template<typename T>
+		T* GetComponent()
+		{
+			return nullptr;
+		}
+
+		template<typename T>
+		Array<T*> GetComponents()
+		{
+			Array<T*> array;
+
+			return array;
+		}
+	};
+}
---
+++ src/Axiom/Framework/Object.hpp
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <Axiom/Core/String.hpp>
+
+namespace Ax
+{
+	typedef uint64_t TTypeID;
+
+	class ObjectBase;
+
+	template <typename Type>
+	struct TypeIDCache
+	{
+		constexpr static const TTypeID Value = HashDjb2(Type::TypeName());
+	};
+
+	class ObjectBase
+	{
+	protected:
+		virtual ~ObjectBase() = default;
+	public:
+		static constexpr const char* TypeName() { return "ObjectBase"; }
+		static TTypeID TypeID() { return TypeIDCache<ObjectBase>::Value; }
+		[[nodiscard]] virtual TTypeID GetTypeID() const = 0;
+		[[nodiscard]] virtual const char* GetTypeName() const { return TypeName(); }
+		[[nodiscard]] virtual bool HasType(TTypeID type) const { return false; }
+
+		template<typename T>
+		[[nodiscard]] bool IsA() const
+		{
+			return HasType(T::TypeID());
+		}
+
+		static ObjectBase* SafeCast(ObjectBase* ptr) { return ptr; }
+		static const ObjectBase* SafeCast(const ObjectBase* ptr) { return ptr; }
+		static ObjectBase* Cast(ObjectBase* ptr) { return ptr; }
+		static ObjectBase& Cast(ObjectBase& ptr) { return ptr; }
+		static const ObjectBase* Cast(const ObjectBase* ptr) { return ptr; }
+		static const ObjectBase& Cast(const ObjectBase& ptr) { return ptr; }
+		static std::shared_ptr<ObjectBase> SafeCast(const std::shared_ptr<ObjectBase>& ptr) { return ptr; }
+		static std::shared_ptr<ObjectBase> Cast(const std::shared_ptr<ObjectBase>& ptr) { return ptr; }
+	};
+
+#define CLASS_OBJ(className, parentClass) \
+    typedef parentClass Super;\
+	static constexpr const char* TypeName() { return #className; } \
+	static TTypeID TypeID() { return TypeIDCache<className>::Value; } \
+	[[nodiscard]] TTypeID GetTypeID() const override { return TypeID(); } \
+	[[nodiscard]] const char* GetTypeName() const override { return TypeName(); } \
+	[[nodiscard]] bool HasType(TTypeID type) const override { if (type == TypeID()) return true; else return parentClass::HasType(type); } \
+	static className* SafeCast(ObjectBase* ptr) \
+	{\
+		if(ptr == nullptr) return nullptr;\
+		if(ptr->HasType(TypeID())) { return static_cast<className*>(ptr); }\
+		else return nullptr;\
+	}\
+	static const className* SafeCast(const ObjectBase* ptr) \
+	{\
+		if(ptr == nullptr) return nullptr;\
+		if(ptr->HasType(TypeID())) { return static_cast<const className*>(ptr); }\
+		else return nullptr;\
+	}\
+	static className* Cast(ObjectBase* ptr) { if(ptr == nullptr) return nullptr; return static_cast<className*>(ptr); }\
+	static className& Cast(ObjectBase& ptr) { return static_cast<className&>(ptr); }\
+	static const className* Cast(const ObjectBase* ptr) { if(ptr == nullptr) return nullptr; return static_cast<const className*>(ptr); }\
+	static const className& Cast(const ObjectBase& ptr) { return static_cast<const className&>(ptr); } \
+	static std::shared_ptr<className> SafeCast(const std::shared_ptr<ObjectBase>& ptr) \
+	{ \
+		if(ptr == nullptr) return nullptr; \
+		if(ptr->HasType(TypeID())) { return static_pointer_cast<className>(ptr); } \
+		else return nullptr; \
+	} \
+	static std::shared_ptr<className> Cast(const std::shared_ptr<ObjectBase>& ptr) { return std::static_pointer_cast<className>(ptr); }
+}
